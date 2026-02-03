4e70f35767d133d2f51931e185955ceb30889fb3
pierclaud7 <pierclaudbouki18@gmail.com>
2026-02-03 04:20:25 +0100

    chore: configuration du main.js avec le router et les styles globaux

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue' // Importe la fonction pour créer une application Vue
+import App from './App.vue' // Importe le composant racine de l'application
+import router from './router' // Importe notre configuration
+import './assets/style.css' // Importe les styles globaux
 
-createApp(App).mount('#app')
+const app = createApp(App) // Crée une nouvelle application Vue avec le composant racine App
+
+app.use(router) // Dit à Vue d'utiliser le système de navigation
+app.mount('#app') // Monte l'application Vue dans l'élément avec l'ID 'app'
